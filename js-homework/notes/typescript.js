c7793c287b215f85f238585a9fc1c49bfe910300
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-12-09 22:49:19 -0800

    commit all again

---
+++ js-homework/notes/typescript.js
@@ -0,0 +1,9 @@
+"use strict";
+function myfunc(params) {
+    console.log("dddd3333");
+}
+myfunc(34);
+class People {
+    constructor() {
+    }
+}
---
+++ js-homework/notes/typescript.ts
@@ -0,0 +1,12 @@
+function myfunc(params:number) {
+    console.log("dddd3333");
+    
+}
+
+myfunc(34);
+
+class People{
+    constructor(){
+        
+    }
+}
