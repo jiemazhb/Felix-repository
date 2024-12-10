b2b757225757a4cd3397681cecac8ea731d233ca
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-12-09 23:54:55 -0800

    build factory design pattern

--- js-homework/notes/typescript.js
+++ js-homework/notes/typescript.js
@@ -1,9 +1,57 @@
 "use strict";
-function myfunc(params) {
-    console.log("dddd3333");
-}
-myfunc(34);
-class People {
-    constructor() {
+const myId = 234;
+const youId = "3f3fw";
+console.log(`myid is ${myId} your id is ${youId}`);
+class Teacher {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    selfIntro() {
+        console.log(`my name is ${this.name} and I am a teacher`);
     }
 }
+class Astronaut {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    selfIntro() {
+        console.log(`my name is ${this.name} and I am a Astronaut`);
+    }
+}
+class Thug {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    selfIntro() {
+        console.log(`my name is ${this.name} and I am a Thug`);
+    }
+}
+class HumanFactory {
+    getHuman(humanType, name = "unknow", age = 1) {
+        if (!humanType) {
+            throw new Error("there is no this type human on earth");
+        }
+        if (humanType.toLowerCase() === "teacher") {
+            return new Teacher(name, age);
+        }
+        else if (humanType.toLowerCase() === "astronaut") {
+            return new Astronaut(name, age);
+        }
+        else if (humanType.toLowerCase() === "thug") {
+            return new Thug(name, age);
+        }
+        throw new Error("unknow human type");
+    }
+    static buildFactory() {
+        return new HumanFactory();
+    }
+}
+const factory = new HumanFactory();
+const t = factory.getHuman("teacher", "Danny");
+t.selfIntro();
+const factory1 = HumanFactory.buildFactory();
+const thug = factory1.getHuman("Thug", "peter");
+thug.selfIntro();
--- js-homework/notes/typescript.ts
+++ js-homework/notes/typescript.ts
@@ -1,12 +1,80 @@
-function myfunc(params:number) {
-    console.log("dddd3333");
-    
+type id = number | string;
+
+const myId : id = 234;
+const youId : id = "3f3fw";
+
+console.log(`myid is ${myId} your id is ${youId}`);
+
+interface Human {
+    name : string;
+    age : number
+    selfIntro(): void;
 }
 
-myfunc(34);
-
-class People{
-    constructor(){
-        
+class Teacher implements Human {
+    name : string;
+    age : number
+    constructor(name : string , age : number){
+        this.name = name;
+        this.age = age;
+    }
+    selfIntro(): void {
+        console.log(`my name is ${this.name} and I am a teacher`);
     }
 }
+
+class Astronaut implements Human {
+    name : string;
+    age : number
+    constructor(name : string , age : number){
+        this.name = name;
+        this.age = age;
+    }
+    selfIntro(): void {
+        console.log(`my name is ${this.name} and I am a Astronaut`);
+    }
+}
+
+class Thug implements Human {
+    name : string;
+    age : number
+    constructor(name : string , age : number){
+        this.name = name;
+        this.age = age;
+    }
+    selfIntro(): void {
+        console.log(`my name is ${this.name} and I am a Thug`);
+    }
+}
+
+class HumanFactory{
+
+    getHuman(humanType : string, name : string = "unknow", age : number = 1): Human{
+        if(!humanType){
+            throw new Error("there is no this type human on earth");
+        }
+
+        if(humanType.toLowerCase() === "teacher"){
+            return new Teacher(name, age);
+        }else if(humanType.toLowerCase() === "astronaut"){
+            return new Astronaut(name, age);
+        }else if(humanType.toLowerCase() === "thug"){
+            return new Thug(name, age);
+        }
+        
+        throw new Error("unknow human type"); 
+    }
+
+    static buildFactory() : HumanFactory{
+        return new HumanFactory();
+    }
+}
+
+const factory = new HumanFactory();
+const t = factory.getHuman("teacher", "Danny");
+t.selfIntro();
+
+const factory1 = HumanFactory.buildFactory();
+const thug = factory1.getHuman("Thug", "peter");
+thug.selfIntro();
+
