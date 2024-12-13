28a91119dda505cdfd07fffe920728be3920dcaa
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-12-12 22:33:58 -0800

    homework13 done

---
+++ js-homework/notes/react-practice/src/Day13/AutoFetch.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+
+export default function AutoFetch() {
+  const [data, setData] = useState(null);
+  const [postId, setpostId] = useState(1);
+  const [errorMsg, seterrorMsg] = useState("");
+
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${postId}`,
+          {
+            method: "GET",
+          }
+        );
+
+        if (response.ok) {
+          const obj = await response.json();
+          setData(obj);
+
+        } else {
+          seterrorMsg("no more post");
+
+        }
+      } catch (error) {
+        seterrorMsg(`getting exception ${error}`);
+
+      }
+    };
+
+    getData();
+  }, [postId]);
+
+  useEffect(() => {
+
+
+    const intervalId = setInterval(() => {
+      
+      setpostId((prevPostId) => {
+        if (errorMsg) return prevPostId; 
+        
+        return prevPostId + 1;       
+      })
+
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [errorMsg]);
+
+  return (
+    <>
+      <ul>
+        {data ? (
+          (() => {
+            const { userId, id, title, body } = data;
+            return (
+              <li>
+                <div>UserId : {userId}</div>
+                <div>Id : {id}</div>
+                <div>Title : {title}</div>
+                <div>Body : {body}</div>
+              </li>
+            );
+          })()
+        ) : (
+          <div>{errorMsg}</div>
+        )}
+      </ul>
+    </>
+  );
+}
--- js-homework/notes/react-practice/src/Day13/Day13.js
+++ js-homework/notes/react-practice/src/Day13/Day13.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import FetchPost from './FetchPost'
 import Btns from './Btns'
+import AutoFetch from './AutoFetch'
 export default function Day13() {
   return (
     <div>
       <h1>fetch posts from jsonplaceholder by id</h1>
       <FetchPost />
-      <hr></hr>
+      <hr style={{marginTop: 40, marginBottom: 40}}></hr>
       <h1>Write a counter timer</h1>
       <Btns />
+      <hr style={{marginTop: 40, marginBottom: 40}}></hr>
       <h1>Fetch posts variation</h1>
+      <AutoFetch/>
     </div>
   )
 }
