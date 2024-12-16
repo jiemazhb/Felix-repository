f11cdf375caf9e66d558877950d9d8a71de38eac
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-12-15 18:28:08 -0800

    update

--- react_homework/src/day11_describing_the_ui/Student.tsx
+++ react_homework/src/day11_describing_the_ui/Student.tsx
@@ -13,14 +13,38 @@ import { Component } from "react";
 
 // implement a Student component here
 
-export interface StudentType {}
-
-export function StudentFn() {
-  return <div data-testid="student"></div>;
+export interface StudentType {
+  id : number
+  name : string 
+  age : number
+  grade :  "A" | "B" | "C" | "D" | "F";
 }
 
-export class StudentClass extends Component {
+export function StudentFn( obj : {student : StudentType}) {
+ 
+  const {student} = obj
+  const {id, name, age, grade} = student
+
+  return (
+    <div data-testid="student">
+      <p>{`${id}`}</p>
+      <p>{`${name}`}</p>
+      <p>{`${age}`}</p>
+      <p>{`${grade}`}</p>
+    </div>
+  );
+}
+
+export class StudentClass extends Component<{ student: StudentType }> {
   render() {
-    return <div data-testid="student">Student</div>;
+    const { student } = this.props;
+    return (
+      <div data-testid="student">
+        <p>{`${student.id}`}</p>
+        <p>{`${student.name}`}</p>
+        <p>{`${student.age}`}</p>
+        <p>{`${student.grade}`}</p>
+      </div>
+    );
   }
 }
